96486e9fdd91dd2087986c4b94d4c540f7e0b626
ugliestcerberus <league@WTS22.attlocal.net>
2017-11-25 15:56:43 -0800

    Binary Converter (IP)

--- src/level1/BInaryConverter.java
+++ src/level1/BInaryConverter.java
@@ -1,5 +1,28 @@
 package level1;
 
-public class BInaryConverter {
+import java.awt.BorderLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class BinaryConverter {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setLayout(new BorderLayout());
+
+		frame.setVisible(true);
+		frame.add(panel);
+		JButton button = new JButton();
+		JTextField input = new JTextField(20);
+		JTextField answer = new JTextField(20);
+		panel.add(input);
+		panel.add(button);
+		panel.add(answer);
+		button.setText("Converts to...");
+		frame.pack();
+
+	}
 }
