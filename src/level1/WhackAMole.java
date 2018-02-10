9980aa2f4e699f0f50a298b480d102b677e7d8c8
codingpanda23 <league@WTS22.attlocal.net>
2018-02-10 15:58:36 -0800

    WhackAMole(IP)
    Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!!
    Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!!
    Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!! Pretzel!!
    Pretzel!! Pretzel!! Pretzel!!

--- src/level1/WhackAMole.java
+++ src/level1/WhackAMole.java
@@ -8,50 +8,56 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class WhackAMole implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	JButton mole = new JButton("Mole");
-public static void main(String[] args) {
-	Random r = new Random();
-	int num = r.nextInt(25);
-	new WhackAMole().createButton(num);
-}
-void createButton(int random) {
-	frame = new JFrame();
-	panel = new JPanel();
-	frame.add(panel);
-	frame.setVisible(true);
-	frame.setSize(250, 300);
-	frame.setTitle("Whack the Mole for Filet-Mignon and Glory");
-	mole.addActionListener(this);
-	
-	for (int i = 0; i < 24; i++) {
-		if (i == random) {
-			panel.add(mole);
-			mole.addActionListener(this);
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int num = r.nextInt(25);
+		new WhackAMole().createButton(num);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == mole) {
+			speak("Supercalifragilisticexpialidocious-ness");
+		} else {
+			speak("You dorky dork dork.");
 		}
-		else {
-			JButton button = new JButton();
-			panel.add(button);
-			button.addActionListener(this);
+		frame.dispose();
+		Random rand = new Random();
+		int num = rand.nextInt(25);
+		new WhackAMole().createButton(num);
+	}
+
+	void createButton(int random) {
+		frame = new JFrame();
+		panel = new JPanel();
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(500, 250);
+		frame.setTitle("Whack the Mole for Filet-Mignon and Glory");
+
+		for (int i = 0; i < 24; i++) {
+			if (i == random) {
+				panel.add(mole);
+				mole.addActionListener(this);
+			} else {
+				JButton button = new JButton();
+				panel.add(button);
+				button.addActionListener(this);
+			}
+		}
+
+	}
+
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
-}
-
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	if (e.getSource().equals(mole)) {
-		speak("You have officialy proven that you are not a dork. Supercalifragilisticexpialidocious-ness");
-	}
-}
-private void speak(String string) {
-	// TODO Auto-generated method stub
-	
-}
-
-
 }
