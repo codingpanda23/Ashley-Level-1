6ccd6c693bc18eeb33a02d34e2b06ce288531baf
codingpanda23 <league@WTS-IM-17.attlocal.net>
2018-05-05 16:00:33 -0700

    stuff

---
+++ src/level1/ShoppingList.java
@@ -0,0 +1,19 @@
+package level1;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ShoppingList {
+public static void main(String[] args) {
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	JButton button = new JButton();
+	
+	frame.setVisible(true);
+	frame.setTitle("Sunfish Shopping");
+	frame.add(panel);
+	
+	
+}
+}
