12fb40c8a94f946f4086364764a5671f93c268d0
loganitis899 <league@WTS20.attlocal.net>
2018-05-12 15:47:39 -0700

    ShoppingList (IP)

--- src/level1/ShoppingList.java
+++ src/level1/ShoppingList.java
@@ -3,16 +3,23 @@ package level1;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ShoppingList {
 public static void main(String[] args) {
-	JPanel panel = new JPanel();
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 	JButton button = new JButton();
+	JTextField field = new JTextField();
 	
 	frame.setVisible(true);
-	frame.setTitle("Sunfish Shopping");
+	frame.setTitle("Buy Stuff!!!");
 	frame.add(panel);
+	panel.add(button);
+	panel.add(field);
+	frame.pack();
+	button.setText("Submit");
+	//ashley iz awsome
 	
 	
 }
